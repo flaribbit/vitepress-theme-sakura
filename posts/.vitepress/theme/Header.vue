948e427f727c7c1cb417fd445b2a634625a44c99
flaribbit <1149761294@qq.com>
2022-02-06 15:54:04 +0800

    init

---
+++ posts/.vitepress/theme/Header.vue
@@ -0,0 +1,59 @@
+<template>
+  <header>
+    <span class="brand"></span>
+    <span class="container">
+      <span class="nav"></span>
+      <span class="menu">
+        <ul>
+          <li v-for="m in menu">
+            <a :href="m.url">
+              <span>
+                <i :class="['fa', m.icon]"></i>
+                {{ m.name }}
+              </span>
+            </a>
+          </li>
+        </ul>
+      </span>
+    </span>
+    <span class="avatar"></span>
+    <span class="search"></span>
+  </header>
+</template>
+
+<script setup lang="ts">
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/css/solid.css'
+const menu = [{
+  icon: 'fa-home',
+  name: '首页',
+  url: '/',
+}, {
+  icon: 'fa-archive',
+  name: '归档',
+  url: '/archive',
+}, {
+  icon: 'fa-list-ul',
+  name: '清单',
+  url: '/list',
+}, {
+  icon: 'fa-leaf',
+  name: '关于',
+  url: '/about',
+}]
+</script>
+
+<style scoped>
+.menu ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+.menu li {
+  margin: 0 12px;
+  display: inline;
+}
+a {
+  color: var(--color-gray);
+}
+</style>
---
+++ posts/.vitepress/theme/Layout.vue
@@ -0,0 +1,25 @@
+<template>
+  <Header />
+  <aside />
+  <main>
+    <Content class="content" />
+  </main>
+</template>
+
+<script setup>
+import Header from './Header.vue'
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+
+  --color-accent: #e67474;
+  --color-gray: #666;
+}
+a {
+  color: var(--color-accent);
+  text-decoration: none;
+}
+</style>
