12be8634e52265b328bdc17e445674946521f403
flaribbit <1149761294@qq.com>
2022-02-15 21:19:56 +0800

    正确的类型标注

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,4 +1,4 @@
-import type { SiteData, HeadConfig } from 'vitepress'
+import { type UserConfig } from 'vitepress'
 export default {
   lang: 'zh-CN',
   base: '/vitepress-theme-sakura/',
@@ -8,5 +8,8 @@ export default {
     ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.0.0/css/regular.min.css' }],
     ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.0.0/css/all.min.css' }],
     ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Serif+SC' }]
-  ] as HeadConfig[],
-} as SiteData
+  ],
+  markdown: {
+    lineNumbers: true
+  }
+} as UserConfig
