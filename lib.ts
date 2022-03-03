ba7e3573f57da462ec65631510e0fc216e8b8f00
flaribbit <1149761294@qq.com>
2022-03-03 16:29:11 +0800

    导出Layout

--- lib.ts
+++ lib.ts
@@ -1,2 +1,6 @@
+// @ts-ignore
+import Layout from './.vitepress/theme/Layout.vue'
+import { DefineComponent } from 'vue'
 export { ThemeConfig } from './.vitepress/config'
+export let Layout: DefineComponent<{}, {}, any>
 export { default } from './.vitepress/theme/index'
