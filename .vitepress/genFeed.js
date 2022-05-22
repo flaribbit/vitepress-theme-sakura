15c1f012d8dffa2ea5cd407aa0ea74e9a85f9fd6
flaribbit <1149761294@qq.com>
2022-05-22 14:28:50 +0800

    更新依赖版本 添加rss脚本 更新版本号

--- .vitepress/genFeed.js
+++ .vitepress/genFeed.js
@@ -21,7 +21,7 @@ resolveSiteData('.').then(siteData => {
   })
 
   load(true).forEach((post) => {
-    const file = path.resolve(cwd, `dist/${post.href}`)
+    const file = path.resolve(cwd, `.vitepress/dist/${post.href}`)
     const rendered = fs.readFileSync(file, 'utf-8')
     const content = rendered.match(
       /<main>([\s\S]*)<\/main>/
@@ -45,5 +45,5 @@ resolveSiteData('.').then(siteData => {
     })
   })
 
-  fs.writeFileSync(path.resolve(cwd, 'dist/feed.rss'), feed.rss2())
+  fs.writeFileSync(path.resolve(cwd, '.vitepress/dist/feed.rss'), feed.rss2())
 })
